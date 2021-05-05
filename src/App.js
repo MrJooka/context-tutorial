36546b3fc3eec23c8aac975558326459ecc50762
Jooka <mr.jooka@gmail.com>
2021-05-05 21:24:54 +0900

    Context {Provider, Context}, 동적 Context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import ColorBox from "./components/ColorBox";
+import SelectColors from "./components/SelectColors";
+import { ColorProvider } from "./contexts/color";
+
 function App() {
-  return <div className="App">Hello React</div>;
+  return (
+    <ColorProvider>
+      <div>
+        <SelectColors />
+        <ColorBox />
+      </div>
+    </ColorProvider>
+  );
 }
 
 export default App;
